3ba9eb0951588a9634c4b798e0d3903e8e3c6956
Erwan BOEHM <erwan.boehm@gmail.com>
2025-10-14 09:13:18 +0200

    test sentry integration

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+import os
+import sentry_sdk
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import APIKeyHeader
 from app.api.routes import router as api_router
 from app.config import config
@@ -13,11 +15,22 @@ async def get_api_key(api_key_header: str = Depends(api_key_header)):
         )
     return api_key_header
 
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DNS"),
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
+
 app = FastAPI(
     title="OCR Backend Service",
     description="Multi-model OCR service with configurable workers and services",
     dependencies=[Depends(get_api_key)]
 )
+
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
 
 app.include_router(api_router)
 
